64f2a798ae9b58f615bdb0b774cf8b564666a4dc
MARVAN18 <marvan_raul@hotmail.com>
2019-02-22 17:19:44 -0600

    Tarea de Modificacion

--- Tarea Eliminacion/app.js
+++ Tarea Eliminacion/app.js
@@ -4,56 +4,58 @@ var app = express();
 
 const archivos = require('fs');
 
+app.use(bodyParser.urlencoded({extenden:false}));
+app.use(bodyParser.json());
 
 //DB Handler
 var db = {
     //Indicar BD o abrir conexion
     initDB: function () {
         var fs = require("fs");
-        var contents = fs.readFileSync("./alumnos.json");
-        this.alumnos = JSON.parse(contents);
+        var contents = fs.readFileSync("./usuarios.json");
+        this.usuarios = JSON.parse(contents);
     },
 
-    //Busqueda Alumno
-    getAlumnoBy: function (filter, value) {
-        console.log("filtro: " + filter + "valor: " + value);
-        var selected = null;
-        this.alumnos.forEach(alumno => {
-            console.log(alumno);
-            console.log(alumno[filter]);
-            if (alumno[filter] == value) {
-                selected = alumno;
-                return selected;
-            }
-        });
-        return selected;
-    },
+    //Busqueda Usuario
+    getUsuarioBy: function (filter, value) {
+      console.log("filtro: " + filter + "valor: " + value);
+      var selected = null;
+      this.usuarios.forEach(usuario => {
+          console.log(usuario);
+          console.log(usuario[filter]);
+          if (usuario[filter] == value) {
+              selected = usuario;
+              return selected;
+          }
+      });
+      return selected;
+  },    
 
-    //Eliminar un alumno por la clave
-    deleteAlumnoByClave : function (clave) {
-      var index;
-      //Buscamos el indice del alumno
-      for (index = 0; index < this.alumnos.length; index++) {
-        if(this.alumnos[index].clave == clave )
-        break;
-      }
-      
-      //Si se encontro el indice se elimina
-      if(index<db.alumnos.length){
-        delete db.alumnos[index];
-        db.alumnos.splice(index, 1);
-        db.saveAlumnos();
-      }
-    },
-
-    saveAlumnos : function(){
-      archivos.writeFileSync('alumnos.json', JSON.stringify(this.alumnos),
+    saveUsuarios : function(){
+      archivos.writeFileSync('usuarios.json', JSON.stringify(this.usuarios),
         function (error) {
             if (error) {
                 console.log('Hubo un error al escribir en el archivo')
                 console.log(error);
             }
         });
+    },
+
+    //Eliminar un usuario por el email
+    deleteAlumnoByEmail : function (email) {
+      var index;
+      //Buscamos el indice del usuario
+      for (index = 0; index < this.usuarios.length; index++) {
+        if(this.usuarios[index].email == email )
+        break;
+      }
+      
+      //Si se encontro el indice se elimina
+      if(index<db.usuarios.length){
+        delete db.usuarios[index];
+        db.usuarios.splice(index, 1);
+        db.saveUsuarios();
+      }
     }
     
 }
@@ -63,39 +65,52 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-  res.sendfile("index.html" );
+  res.sendFile('index.html' , { root : __dirname});
 });
 
-app.route("/alumnos")
+app.route("/usuarios")
   .get( (req, res) => {
     db.initDB();
-    res.json(db.alumnos);
+    res.json(db.usuarios);
   })
   .post((req,res)=> {
     db.initDB();
-    var alumno = req.body;
+    var usuario = req.body;
     console.log("Objeto post recibido");
-    console.log(alumno);
-    db.alumnos.push(alumno);
-    db.saveAlumnos();
+    console.log(usuario);
+    db.usuarios.push(usuario);
+    db.saveUsuarios();
     res.json({'status' : 'OK'});
   })
   .delete((req,res)=> {
     db.initDB();
-    var alumno = req.body;
+    var usuario = req.body;
     console.log("Objeto post recibido");
-    console.log(alumno);
-    db.deleteAlumnoByClave( alumno.clave );
-    console.log(db.alumnos);
+    console.log(usuario);
+    db.deleteAlumnoByEmail( usuario.email);
+    console.log(db.usuarios);
     res.json({'status' : 'OK'})
   });
 
-app.get('/alumnos/:clave', (req, res) => {
-  db.initDB();
-  var clave = req.params.clave;
-  var alumno = db.getAlumnoBy('clave', clave);
-  res.json(alumno);
-});
+  app.get('/usuario/:email', (req, res) => {
+    db.initDB();
+    var email = req.params.email;
+    var usuario = db.getUsuarioBy('email', email);
+    res.json(usuario);
+  });
+
+  app.put('/usuario/:email',(req,res) => {
+    //
+    db.initDB();
+    var email = req.params.emailN;
+    var pass = req.params.passN;
+    
+    console.log("Objeto post recibido");
+    console.log(usuario);
+    db.deleteAlumnoByEmail( usuario.email);
+    console.log(db.usuarios);
+    res.json({'status' : 'OK'})
+  });
 
 app.listen(3000,function(){
   console.log("Started on PORT 3000");
